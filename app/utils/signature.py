605346c3a63448f8a6fedeaca8e3c2d469c715e4
RektPunk <rektpunk@gmail.com>
2025-05-04 22:31:55 +0900

    add input name

--- app/utils/signature.py
+++ app/utils/signature.py
@@ -6,7 +6,9 @@ import hmac
 def verify_signature(body: str, received_signature: str, secret: str) -> bool:
     """Verify the HMAC signature of the request body."""
     hash_digest = hmac.new(
-        secret.encode("utf-8"), body.encode("utf-8"), hashlib.sha256
+        key=secret.encode("utf-8"),
+        msg=body.encode("utf-8"),
+        digestmod=hashlib.sha256,
     ).digest()
     expected_signature = base64.b64encode(hash_digest).decode("utf-8")
     return hmac.compare_digest(expected_signature, received_signature)
